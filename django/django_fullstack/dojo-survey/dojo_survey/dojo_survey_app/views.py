a8518964aaf772282c69350c02901ca5ab8683bb
abdo-ss1 <abdo-ss1@hotmail.com>
2026-03-04 01:26:29 +0200

    dojo-survey assignment solved

---
+++ django/django_fullstack/dojo-survey/dojo_survey/dojo_survey_app/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    return render(request, "index.html")
+
+def result(request):
+    context = {
+        'name': request.POST.get('name'),
+        'location': request.POST.get("location"),
+        'favorite_language': request.POST.get("favorite_language"),
+        'comment': request.POST.get("comment")
+    }
+    return render(request, "result.html", context)
+    
