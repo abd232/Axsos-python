b35a1c6701dee89f70449c76b7167928bdff3760
abdo-ss1 <abdo-ss1@hotmail.com>
2026-03-03 20:41:17 +0200

    added project and django pip

---
+++ django/django_fundamentals/first-django-project/first_django_project/first_django_projectApp/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render, redirect, HttpResponse
+from django.http import JsonResponse
+# Create your views here.
+def root(request):
+    return redirect('/blogs')
+
+def index(request):
+    return HttpResponse("placeholder to later display a list of all blogs")
+
+def new(request):
+    return HttpResponse("placeholder to display a new form to create a new blog with a method named 'create'")
+
+def create(request):
+    return redirect('/')
+
+def show(request, num):
+    return HttpResponse(f"placeholder to display blog number: {num}")
+
+def edit(request, num):
+    return HttpResponse(f"placeholder to edit blog {num}")
+
+def destroy(request, num):
+    return redirect('/')
+
+def json(request):
+    return JsonResponse({'title': "blogTitle", 'content': "something here"})
